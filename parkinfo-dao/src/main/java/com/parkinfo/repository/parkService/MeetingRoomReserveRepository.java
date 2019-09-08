8f2fc5f6f97f168a5793c9233cff4e087438ff74
chenlong <chenlong_qrr@163.com>
2019-09-08 12:38:05 +0800

    会议室修改

--- parkinfo-dao/src/main/java/com/parkinfo/repository/parkService/MeetingRoomReserveRepository.java
+++ parkinfo-dao/src/main/java/com/parkinfo/repository/parkService/MeetingRoomReserveRepository.java
@@ -9,17 +9,17 @@ import java.util.List;
 
 public interface MeetingRoomReserveRepository extends JpaRepository<MeetingRoomReserve, String> {
 
-    @Query(nativeQuery = true, value = "SELECT count(*) FROM c_ser_meeting_room_reserve re WHERE re.meeting_room_id = ? 1 AND re.`delete` = 0 AND re.available = 1\n" +
+    @Query(nativeQuery = true, value = "SELECT count(*) FROM c_ser_meeting_room_reserve re WHERE re.meeting_room_id = ?1 AND re.`delete` = 0 AND re.available = 1\n" +
             "AND (\n" +
-            "\t(date_format(? 2, '%Y-%m-%d %H:%i:%s') BETWEEN date_format(re.start_time,'%Y-%m-%d %H:%i:%s')\n" +
+            "\t(date_format(?2, '%Y-%m-%d %H:%i:%s') BETWEEN date_format(re.start_time,'%Y-%m-%d %H:%i:%s')\n" +
             "\t\tAND date_format(re.end_time,'%Y-%m-%d %H:%i:%s'))\n" +
             "\tOR (\n" +
-            "\t\tdate_format(? 3, '%Y-%m-%d %H:%i:%s') BETWEEN date_format(re.start_time,'%Y-%m-%d %H:%i:%s')\n" +
+            "\t\tdate_format(?3, '%Y-%m-%d %H:%i:%s') BETWEEN date_format(re.start_time,'%Y-%m-%d %H:%i:%s')\n" +
             "\t\tAND date_format(re.end_time,'%Y-%m-%d %H:%i:%s')\n" +
             "\t)\n" +
             "\tOR (\n" +
-            "\t\tdate_format(re.end_time,'%Y-%m-%d %H:%i:%s') BETWEEN date_format(? 2, '%Y-%m-%d %H:%i:%s')\n" +
-            "\t\tAND date_format(? 3, '%Y-%m-%d %H:%i:%s')\n" +
+            "\t\tdate_format(re.end_time,'%Y-%m-%d %H:%i:%s') BETWEEN date_format(?2, '%Y-%m-%d %H:%i:%s')\n" +
+            "\t\tAND date_format(?3, '%Y-%m-%d %H:%i:%s')\n" +
             "\t)\n" +
             ")")
     Integer findMeetingRoomReserve(String roomId, Date startTime, Date endTime);
