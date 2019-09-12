3450b90fadba06f48e22298edc5b10b62a212ad8
DragonChenCL <chenlong_qrr@163.com>
2019-09-12 17:49:27 +0800

    服务需求信息填报

--- parkinfo-service/src/main/java/com/parkinfo/service/parkService/impl/ServiceInfoServiceImpl.java
+++ parkinfo-service/src/main/java/com/parkinfo/service/parkService/impl/ServiceInfoServiceImpl.java
@@ -1,8 +1,40 @@
 package com.parkinfo.service.parkService.impl;
 
+import com.parkinfo.common.Result;
+import com.parkinfo.dto.ParkUserDTO;
+import com.parkinfo.entity.companyManage.CompanyDetail;
+import com.parkinfo.entity.companyManage.EnclosureTotal;
+import com.parkinfo.enums.EnclosureType;
+import com.parkinfo.exception.NormalException;
+import com.parkinfo.repository.companyManage.CompanyDetailRepository;
+import com.parkinfo.response.parkService.CompanyDataResponse;
 import com.parkinfo.service.parkService.IServiceInfoService;
+import com.parkinfo.token.TokenUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceInfoServiceImpl implements IServiceInfoService {
+    @Autowired
+    private CompanyDetailRepository companyDetailRepository;
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    @Override
+    public Result<CompanyDataResponse> getCompanyDataResponse() {
+        ParkUserDTO loginUserDTO = tokenUtils.getLoginUserDTO();
+        CompanyDataResponse response = new CompanyDataResponse();
+        Optional<CompanyDetail> companyDetailOptional = companyDetailRepository.findByDeleteIsFalseAndParkUser_Id(loginUserDTO.getId());
+        companyDetailOptional.ifPresent(companyDetail -> {
+            BeanUtils.copyProperties(companyDetail,response);
+            Set<EnclosureTotal> collect = companyDetail.getEnclosureTotals().stream().filter(enclosureTotal -> enclosureTotal.getDelete() == Boolean.FALSE && enclosureTotal.getEnclosureType().equals(EnclosureType.SUPPLEMENT)).collect(Collectors.toSet());
+            response.setEnclosureTotals(collect);
+        });
+        return Result.<CompanyDataResponse>builder().success().data(response).build();
+    }
 }
