681713b8dca0e1c5ec857740c9a83363ab67608f
chenlong <chenlong_qrr@163.com>
2019-09-21 15:33:38 +0800

    会议室预约增加字段

--- parkinfo-entity/src/main/java/com/parkinfo/response/parkService/MeetingRoomResponse.java
+++ parkinfo-entity/src/main/java/com/parkinfo/response/parkService/MeetingRoomResponse.java
@@ -1,7 +1,9 @@
 package com.parkinfo.response.parkService;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.apache.poi.hpsf.Date;
 
 import java.util.List;
 
@@ -27,4 +29,8 @@ public class MeetingRoomResponse {
 
     @ApiModelProperty(value = "创建人名称")
     private String userName;
+
+    @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
+    private Date createTime;
 }
