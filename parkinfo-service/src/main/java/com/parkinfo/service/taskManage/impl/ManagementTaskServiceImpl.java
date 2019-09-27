c04e34e89e2a0d64ad1b39a8b0e31cfeefea6150
yuchu <cnyuchu@gmail.com>
2019-09-27 18:00:43 +0800

    bug fixed

--- parkinfo-service/src/main/java/com/parkinfo/service/taskManage/impl/ManagementTaskServiceImpl.java
+++ parkinfo-service/src/main/java/com/parkinfo/service/taskManage/impl/ManagementTaskServiceImpl.java
@@ -58,11 +58,8 @@ public class ManagementTaskServiceImpl implements IManagementTaskService {
         Pageable pageable = PageRequest.of(request.getPageNum(), request.getPageSize(), Sort.Direction.DESC, "createTime");
         Specification<ManagementTask> managementTaskSpecification = (Specification<ManagementTask>) (root, criteriaQuery, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-            if (request.getStartTimeFrom() != null && request.getStartTimeTo() != null) {
-                predicates.add(criteriaBuilder.between(root.get("startTime"), request.getStartTimeFrom(), request.getStartTimeTo()));
-            }
-            if (request.getEndTimeFrom() != null && request.getEndTimeTo() != null) {
-                predicates.add(criteriaBuilder.between(root.get("endTime"), request.getEndTimeFrom(), request.getEndTimeTo()));
+            if (request.getCreateTimeFrom() != null && request.getCreateTimeTo() != null) {
+                predicates.add(criteriaBuilder.between(root.get("createTime"), request.getCreateTimeFrom(), request.getCreateTimeTo()));
             }
             if (StringUtils.isNotBlank(request.getName())) {
                 predicates.add(criteriaBuilder.like(root.get("name").as(String.class), "%" + request.getName() + "%"));
