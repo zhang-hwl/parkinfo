39146a9aedca1fea6d416c349da764fab6c8741b
DragonChenCL <chenlong_qrr@163.com>
2019-09-29 09:45:19 +0800

    会议室

--- parkinfo-dao/src/main/java/com/parkinfo/repository/parkService/MeetingRoomRepository.java
+++ parkinfo-dao/src/main/java/com/parkinfo/repository/parkService/MeetingRoomRepository.java
@@ -1,10 +1,15 @@
 package com.parkinfo.repository.parkService;
 
 import com.parkinfo.entity.parkService.meetingRoom.MeetingRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MeetingRoomRepository extends JpaRepository<MeetingRoom,String> {
     Optional<MeetingRoom> findFirstByDeleteIsFalseAndAvailableIsTrueAndId(String roomId);
+
+    Page<MeetingRoom> findAll(Specification<MeetingRoom> specification, Pageable pageable);
 }
