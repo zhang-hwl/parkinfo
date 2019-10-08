70ccb71939cbb8b5ec9f6c7602b509063450584e
libin <libin11@163.com>
2019-10-08 11:07:14 +0800

    模板管理增加Swagger测试接口

--- parkinfo-service/src/main/java/com/parkinfo/service/upload/IUploadService.java
+++ parkinfo-service/src/main/java/com/parkinfo/service/upload/IUploadService.java
@@ -1,11 +1,14 @@
 package com.parkinfo.service.upload;
 
 import com.parkinfo.common.Result;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface IUploadService {
 
     Result<String> FileUpload(HttpServletRequest request);
+
+    Result<String> fileTest(MultipartFile file);
 
 }
--- parkinfo-service/src/main/java/com/parkinfo/service/upload/impl/UploadServiceImpl.java
+++ parkinfo-service/src/main/java/com/parkinfo/service/upload/impl/UploadServiceImpl.java
@@ -6,6 +6,7 @@ import com.parkinfo.token.TokenUtils;
 import com.parkinfo.tools.oss.IOssService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,6 +22,13 @@ public class UploadServiceImpl implements IUploadService {
     public Result<String> FileUpload(HttpServletRequest request) {
         String realPath = "parkInfo";
         String url = ossService.MultipartFileUpload(request, realPath);
+        return Result.<String>builder().success().data(url).build();
+    }
+
+    @Override
+    public Result<String> fileTest(MultipartFile file) {
+        String realPath = "parkInfo";
+        String url = ossService.fileUploadTest(file, realPath);
         return Result.<String>builder().success().data(url).build();
     }
 }
