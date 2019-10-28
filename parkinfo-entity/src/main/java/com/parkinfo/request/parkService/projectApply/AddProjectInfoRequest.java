502f3031e0c82c25a4c38422b8ebec53584ee943
libin <libin11@163.com>
2019-10-28 17:15:57 +0800

    项目申报加参数校验

--- parkinfo-entity/src/main/java/com/parkinfo/request/parkService/projectApply/AddProjectInfoRequest.java
+++ parkinfo-entity/src/main/java/com/parkinfo/request/parkService/projectApply/AddProjectInfoRequest.java
@@ -3,9 +3,13 @@ package com.parkinfo.request.parkService.projectApply;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class AddProjectInfoRequest {
+
     @ApiModelProperty("项目名称")
+    @NotBlank(message = "项目名称不能为空")
     private String projectName;
 
     @ApiModelProperty("项目奖励")
@@ -15,5 +19,6 @@ public class AddProjectInfoRequest {
     private String projectRequire;
 
     @ApiModelProperty("分类id")
+    @NotBlank(message = "项目类型不能为空")
     private String typeId;
 }
